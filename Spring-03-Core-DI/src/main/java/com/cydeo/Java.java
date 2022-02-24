9a3de574f9fc5046b0e6203d54c69039628ffa32
RumCake01 <mia.hasnutdinova@gmail.com>
2022-02-23 21:39:25 -0500

    @AllArgsConstuctor is used and no need for @Wirid

--- Spring-03-Core-DI/src/main/java/com/cydeo/Java.java
+++ Spring-03-Core-DI/src/main/java/com/cydeo/Java.java
@@ -1,26 +1,28 @@
 package com.cydeo;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor  // with ths constructor we dont need to provide @Autowired annotation
 public class Java {
 
-    // field injection
+    // field injection is not recommended
 //    @Autowired
 //    OfficeHours officeHours;
 
 
-    // COnstructor injection
-
+    // COnstructor injection, but instead we used ->
+    // @AllArgumenrsConstructor because we have only one constructor
     OfficeHours officeHours;
-    @Autowired
-    public Java(OfficeHours officeHours){
-        this.officeHours = officeHours;
-    }
+//    @Autowired
+//    public Java(OfficeHours officeHours){
+//        this.officeHours = officeHours;
+//    }
 
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours: " + (20 + officeHours.getHours()));
+        System.out.println("Weekly teaching hours: " + officeHours.getHours());
 
     }
 }
