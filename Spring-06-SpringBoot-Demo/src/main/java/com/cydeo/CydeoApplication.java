58df1c44ca2af673886781de96f0422e3c07cfb6
RumCake01 <mia.hasnutdinova@gmail.com>
2022-03-01 19:30:10 -0500

    Spring Boot packages intro

--- Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -1,13 +1,31 @@
 package com.cydeo;
 
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
 
 @SpringBootApplication
 public class CydeoApplication {
 
+
+
     public static void main(String[] args) {
-        SpringApplication.run(CydeoApplication.class, args);
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Crime and Punishment");
+
+      ApplicationContext context =  SpringApplication.run(CydeoApplication.class, args);
+
+        CommentService cs = context.getBean(CommentService.class);
+        cs.publishComment(comment);
+
+
+
     }
 
 }
