dd636917a3b44eb7a0dc2bfc5f9cc5d97eb69300
RumCake01 <mia.hasnutdinova@gmail.com>
2022-03-01 21:37:12 -0500

    MVC - Model

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,17 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping("/welcome")
+    public String homepage(Model model){ // method parameter.
+        model.addAttribute("name", "Cydeo"); // takes two parameters
+        model.addAttribute("course", "MVC");
+
+        return "student/welcome" ;
+    }
+}
